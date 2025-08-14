70e30e21c27e4ff52caaddb28f6a3c25e0b5c0b7
Kyle Cuss <cussprogramming@gmail.com>
2025-08-14 14:43:34 -0230

    Setup frame for enhanced player input

---
+++ Source/AuraComplete/Private/Player/AuraPlayerController.cpp
@@ -0,0 +1,31 @@
+// Copyright Cuss Programming
+
+
+#include "Player/AuraPlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+AAuraPlayerController::AAuraPlayerController()
+{
+	bReplicates = true;
+}
+
+void AAuraPlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	check(AuraInputContext);
+
+	//Add mapping context to player
+	UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	check(Subsystem);
+	Subsystem->AddMappingContext(AuraInputContext, 0);
+
+	//Show mouse cursor during gameplay
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+
+	//Do not lock mouse cursor during play
+	FInputModeGameAndUI InputModeData;
+	InputModeData.SetLockMouseToViewportBehavior(EMouseLockMode::DoNotLock);
+	InputModeData.SetHideCursorDuringCapture(false);
+	SetInputMode(InputModeData);
+}
