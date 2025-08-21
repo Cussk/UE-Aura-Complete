523b9dd84f82f7eb633cc35cd4a48413a0fdc0d8
Kyle Cuss <cussprogramming@gmail.com>
2025-08-21 09:08:29 -0230

    Setup Mana/MaxMana delegate broadcasting

--- Source/AuraComplete/Private/UI/WidgetController/OverlayWidgetController.cpp
+++ Source/AuraComplete/Private/UI/WidgetController/OverlayWidgetController.cpp
@@ -9,25 +9,27 @@ void UOverlayWidgetController::BroadcastInitialValues()
 	const UAuraAttributeSet* AuraAttributes = CastChecked<UAuraAttributeSet>(AttributeSet);
 	OnHealthChanged.Broadcast(AuraAttributes->GetHealth());
 	OnMaxHealthChanged.Broadcast(AuraAttributes->GetMaxHealth());
+	OnManaChanged.Broadcast(AuraAttributes->GetMana());
+	OnMaxManaChanged.Broadcast(AuraAttributes->GetMaxMana());
 }
 
 void UOverlayWidgetController::BindCallbacksToDependencies()
 {
 	const UAuraAttributeSet* AuraAttributes = CastChecked<UAuraAttributeSet>(AttributeSet);
-	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributes->GetHealthAttribute())
-	.AddUObject(this, &UOverlayWidgetController::HealthChanged);
 
-	AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(AuraAttributes->GetMaxHealthAttribute())
-	.AddUObject(this, &UOverlayWidgetController::MaxHealthChanged);
-}
+	//Generic bind for attributes
+	auto Bind = [this](const FGameplayAttribute& Attribute, auto& BroadcastDelegate)
+	{
+		AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(Attribute)
+		.AddLambda([&BroadcastDelegate](const FOnAttributeChangeData& Data)
+		{
+			BroadcastDelegate.Broadcast(Data.NewValue);
+		});
+	};
 
-void UOverlayWidgetController::HealthChanged(const FOnAttributeChangeData& Data) const
-{
-	OnHealthChanged.Broadcast(Data.NewValue);
-}
-
-void UOverlayWidgetController::MaxHealthChanged(const FOnAttributeChangeData& Data) const
-{
-	OnMaxHealthChanged.Broadcast(Data.NewValue);
+	Bind(AuraAttributes->GetHealthAttribute(), OnHealthChanged);
+	Bind(AuraAttributes->GetMaxHealthAttribute(), OnMaxHealthChanged);
+	Bind(AuraAttributes->GetManaAttribute(), OnManaChanged);
+	Bind(AuraAttributes->GetMaxManaAttribute(), OnMaxManaChanged);
 }
 
--- Source/AuraComplete/Public/UI/WidgetController/OverlayWidgetController.h
+++ Source/AuraComplete/Public/UI/WidgetController/OverlayWidgetController.h
@@ -10,6 +10,8 @@
 struct FOnAttributeChangeData;
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnHealthChangedSignature, float, NewHealth);
 DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxHealthChangedSignature, float, NewMaxHealth);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnManaChangedSignature, float, NewMana);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnMaxManaChangedSignature, float, NewMaxMana);
 
 /**
  * 
@@ -29,7 +31,10 @@ public:
 	UPROPERTY(BlueprintAssignable, Category = "GAS|Attributes")
 	FOnMaxHealthChangedSignature OnMaxHealthChanged;
 
-protected:
-	void HealthChanged(const FOnAttributeChangeData& Data) const;
-	void MaxHealthChanged(const FOnAttributeChangeData& Data) const;
+	UPROPERTY(BlueprintAssignable, Category = "GAS|Attributes")
+	FOnManaChangedSignature OnManaChanged;
+
+	UPROPERTY(BlueprintAssignable, Category = "GAS|Attributes")
+	FOnMaxManaChangedSignature OnMaxManaChanged;
+	
 };
