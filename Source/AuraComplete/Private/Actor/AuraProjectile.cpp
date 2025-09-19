b01f6f057edceb75e51b6b11a2d2aaf2b13b9fa7
Kyle Cuss <cussprogramming@gmail.com>
2025-09-19 14:47:23 -0230

    Created range attack for Slingshot Goblin

--- Source/AuraComplete/Private/Actor/AuraProjectile.cpp
+++ Source/AuraComplete/Private/Actor/AuraProjectile.cpp
@@ -9,6 +9,7 @@
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "Kismet/GameplayStatics.h"
 #include "NiagaraFunctionLibrary.h"
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
 #include "AuraComplete/AuraComplete.h"
 #include "Components/AudioComponent.h"
 
@@ -62,8 +63,13 @@ void AAuraProjectile::Destroyed()
 void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
 	UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
 {
-	if (!DamageEffectSpecHandle.Data.IsValid() || DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser() == OtherActor) return;
-
+	if (!DamageEffectSpecHandle.Data.IsValid()
+		|| DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser() == OtherActor
+		|| UAuraAbilitySystemLibrary::IsAlly(DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser(), OtherActor))
+	{
+		return;
+	}
+	
 	if (!bHit)
 	{
 		UGameplayStatics::PlaySoundAtLocation(this, ImpactSound, GetActorLocation(), FRotator::ZeroRotator);
