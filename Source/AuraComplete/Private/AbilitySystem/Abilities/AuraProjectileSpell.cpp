64c439a1bed5cba4b94fbb7d2502ecb593cfce01
Kyle Cuss <cussprogramming@gmail.com>
2025-09-25 12:35:02 -0230

    Created ranged type demon enemy.  Fixed Rock projectile gravity to fly further

--- Source/AuraComplete/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/AuraComplete/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -23,12 +23,12 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
  * - Spawns a projectile at the combat socket location of the avatar
  * - Projectile is owned by the ability's owning actor
  */
-void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocation)
+void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag)
 {
 	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
 	if (!bIsServer) return;
 	
-	const FVector SocketLocation =ICombatInterface::Execute_GetCombatSocketLocation(GetAvatarActorFromActorInfo(), FAuraGameplayTags::TAG_CombatSocket_Weapon);
+	const FVector SocketLocation =ICombatInterface::Execute_GetCombatSocketLocation(GetAvatarActorFromActorInfo(), SocketTag);
 	FRotator ProjectileRotation = (ProjectileTargetLocation - SocketLocation).Rotation();
 
 	FTransform SpawnTransform;
